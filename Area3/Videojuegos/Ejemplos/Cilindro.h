176f16fc51da088b640d6f8e9e08cdd8f3a4bc3b
Jesús <Jahl-v2@hotmail.com>
2017-10-13 21:56:06 -0500

    Update Cilindro.h

--- Area3/Videojuegos/Ejemplos/Cilindro.h
+++ Area3/Videojuegos/Ejemplos/Cilindro.h
@@ -1,47 +1,41 @@
 void Cilindro(Slices, Radio, Alto) { 
-		int index = 0;
-		int indexbase = 0;
-		int indextapa = 0;
-		const float phix = D3DX_PI * 2.0;
+  int index = 0;
+  int indexbase = 0;
+  int indextapa = 0;
+  const float phix = D3DX_PI * 2.0;
 
-		SimpleVertex *vertex = new SimpleVertex[Slices * cantOfSecctions];
+  SimpleVertex *vertex = new SimpleVertex[Slices * cantOfSecctions];
+  for (int i = 0; i < cantOfSecctions; i++) {
+    for (int j = 0; j < Slices; j++) {
+      float x = (float)j / (Slices - 1);
+      switch(i) {
+        case 0:
+	  vertex[index].pos.x = Radio * cos(x*phix);
+	  vertex[index].pos.y = -Alto;
+	  vertex[index].pos.z = Radio * sin(x*phix);
+	  indexbase = index;
+	break;
 
-		for (int i = 0; i < cantOfSecctions; i++)
-		{
-			for (int j = 0; j < Slices; j++)
-			{
-				float x = (float)j / (Slices - 1);
-				
-				switch(i)
-				{
-				case 0:
-					vertex[index].pos.x = Radio * cos(x*phix);
-					vertex[index].pos.y = -Alto;
-					vertex[index].pos.z = Radio * sin(x*phix);
+        case 1:
+	  vertex[index].pos.x = Radio * cos(x*phix);
+	  vertex[index].pos.y = Alto;
+	  vertex[index].pos.z = Radio * sin(x*phix);
+	  indextapa = index;
+	break;
 
-					indexbase = index;
-					break;
+        case 2:
+          vertex[index].pos.x = 0;
+	  vertex[index].pos.y = -Alto;
+	  vertex[index].pos.z = 0;
+	break;
 
-				case 1:
-					vertex[index].pos.x = Radio * cos(x*phix);
-					vertex[index].pos.y = Alto;
-					vertex[index].pos.z = Radio * sin(x*phix);
-					indextapa = index;
-					break;
-
-				case 2:
-					vertex[index].pos.x = 0;
-					vertex[index].pos.y = -Alto;
-					vertex[index].pos.z = 0;
-					break;
-
-				case 3:
-					vertex[index].pos.x = 0;
-					vertex[index].pos.y = Alto;
-					vertex[index].pos.z = 0;
-					break;
-				}
-				index++;
-			}
-		}
+	case 3:
+	  vertex[index].pos.x = 0;
+	  vertex[index].pos.y = Alto;
+	  vertex[index].pos.z = 0;
+	break;
+      }
+      index++;
+    }
+  }
 }
